b2c012af8ba2122d3be51fd9c65e14ad469e24ab
Reshma <reshma.chaudhari@dakshabhi.com>
2021-01-14 12:32:12 +0530

    local

--- net/mux/mux.go
+++ net/mux/mux.go
@@ -22,8 +22,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/fatedier/golib/errors"
-	gnet "github.com/fatedier/golib/net"
+	"github.com/vaibhav-panmand/golib/errors"
+	gnet "github.com/vaibhav-panmand/golib/net"
 )
 
 const (
